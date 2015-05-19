d9cfb28e01e7f117584db1f9dbd523ee761a18eb
benjaminsievers <benjaminsievers@seattleacademy.org>
2015-05-19 08:36:38 -0700

    add app.js index.js

---
+++ app.js
@@ -0,0 +1,4 @@
+var index = require('./index.js');
+console.log( 'The area of a circle of radius 4 is '
+           + circle.area(4));
+console.log('The circumference of a circle with radius 4 is',cirle.circumference(4));
